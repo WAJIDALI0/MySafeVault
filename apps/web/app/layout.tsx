96058a51825bec10a2e66a4380a87ee69bd0e48e
Wajid Ali <wajidali3520400@gmail.com>
2026-08-23 11:19:17 -0700

    feat(ui): set default theme to dark

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
